6f22e390cb9c1a092ee756bc28e84e4e1f7f2f5b
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-10 12:40:14 +0400

    GetMapping Done

--- Bank-System/src/main/java/com/Bank/System/Controllers/CustomerController.java
+++ Bank-System/src/main/java/com/Bank/System/Controllers/CustomerController.java
@@ -3,9 +3,12 @@ package com.Bank.System.Controllers;
 import com.Bank.System.Entities.Customer;
 import com.Bank.System.Services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
@@ -16,5 +19,8 @@ public class CustomerController {
     public Customer addCustomer(@RequestBody Customer customer) {
         return customerService.addCustomer(customer);
     }
-
+    @GetMapping("getAll")
+    public List<Customer> getAll() {
+        return customerService.getAllCustomers();
+    }
 }
