f2d5c7f9f27cc0186a3c75a3f250fc5c4d82a4cf
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-10 14:29:52 +0400

    Addition in customer class

--- Bank-System/src/main/java/com/Bank/System/Entities/Customer.java
+++ Bank-System/src/main/java/com/Bank/System/Entities/Customer.java
@@ -1,10 +1,12 @@
 package com.Bank.System.Entities;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.validation.annotation.Validated;
 
 @AllArgsConstructor
 @Data
@@ -14,6 +16,7 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int customerId;
+    @Nonnull
     private String customerName;
     private int accountNumber;
     private int balance;
