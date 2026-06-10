e3904d315d86e493cc6964c53cf3496b6ba0de54
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-10 10:47:43 +0400

    customerRepository is created

---
+++ Bank-System/src/main/java/com/Bank/System/Repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.Bank.System.Repository;
+
+import com.Bank.System.Entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+
+}
