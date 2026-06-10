0b0de85091021b2135854d76521780b47e9df493
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-10 11:30:04 +0400

    Methods are added in the customerRepository

--- Bank-System/src/main/java/com/Bank/System/Repository/CustomerRepository.java
+++ Bank-System/src/main/java/com/Bank/System/Repository/CustomerRepository.java
@@ -2,9 +2,20 @@ package com.Bank.System.Repository;
 
 import com.Bank.System.Entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+    @Query("SELECT c FROM Customer c WHERE c.isActive=true")
+    List<Customer> getAllCustomers();
 
+    @Query("SELECT c from Customer c WHERE c.isActive=true AND c.id=:customerId")
+    Customer getCustomerById(@Param("customerId") Integer id);
+
+    @Query(" SELECT c from Customer c WHERE c.isActive=true AND c.name=:customerName")
+    Customer getTraineeByName(@Param("customerName") String name);
 }
