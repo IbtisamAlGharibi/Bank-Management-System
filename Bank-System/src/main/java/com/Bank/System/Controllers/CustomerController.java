b2a31ceb62c0450d53ae2abf913e7059b19f577d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-10 12:30:09 +0400

    PostMapping Done

--- Bank-System/src/main/java/com/Bank/System/Controllers/CustomerController.java
+++ Bank-System/src/main/java/com/Bank/System/Controllers/CustomerController.java
@@ -1,9 +1,20 @@
 package com.Bank.System.Controllers;
 
+import com.Bank.System.Entities.Customer;
+import com.Bank.System.Services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
 
+    @PostMapping
+    public Customer addCustomer(@RequestBody Customer customer) {
+        return customerService.addCustomer(customer);
+    }
 
 }
